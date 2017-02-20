f637fa7350dc37969cc18f7049043d230cb6d1e2
Jeremy <k2990946@mvrht.com>
2017-02-20 16:22:05 -0600

    first commit

---
+++ pycrypt.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+import random
+
+def encrypt():
+        key = [];
+
+        messageIn = raw_input("what is it that you want to encrypt?:");
+        sliced = list(messageIn);
+
+        for i in sliced:
+            print 1;
+
+        return
+    
+encrypt();
