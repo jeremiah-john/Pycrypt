e373079fa0b353e83a573a961a96c933bce07609
Jeremy <jj399372@011-a218.local>
2017-02-21 12:40:16 -0600

    ord should be used instead of chr, rookie mistake

--- pycrypt.py
+++ pycrypt.py
@@ -6,11 +6,13 @@ def encrypt():
 
         messageIn = raw_input("what is it that you want to encrypt?:");
         sliced = list(messageIn);
+        a = 0
 
         for letter in sliced:
             newKeyEntry = random.randint(1,110);
             key.append(newKeyEntry);
-            print chr(sliced[letter]);
+            print ord(sliced[a]);
+            a = a + 1;
            
         return
 
