f92e4a748cb45f156c7f83f90d6a0043bb118341
Jeremiah John <jeremiahjohn08706@gmail.com>
2017-02-22 22:47:49 -0600

    need to get rid of brackets in final text string

--- pycrypt.py
+++ pycrypt.py
@@ -6,9 +6,11 @@ def encrypt():
 
         messageIn = raw_input("what is it that you want to encrypt?:");
         sliced = list(messageIn); #We now can manipulate the string by letter. we need this for encryption with random integers
-        a = 0 # variable is set so we can go through each letter and assign it a key in for loop
-        manipulate = [] # We keep the numbers we get from ord() here
-        prechr =[]
+        a = 0;  # variable is set so we can go through each letter and assign it a key in for loop
+        manipulate = []; # We keep the numbers we get from ord() here
+
+        prechr = []
+        crypted = []
 
         for letter in sliced:
             newKeyEntry = random.randint(1,110);
@@ -22,10 +24,21 @@ def encrypt():
         for data in manipulate :
             newNum = manipulate[a] + key[a]; # we add the numbers so we can convert them back using chr()
             if newNum > 255:
-                print ("woah there the number is too long,brother!"); #placeholder. need to figure out how to properly get a new number between 1 and 255
-            
-            
-           
+                while newNum > 255:
+                        newNum = newNum - 255;
+            prechr.append(newNum);
+            a = a + 1;
+
+        a = 0
+
+        for number in prechr:  #now we convert each new number into an ASCII character
+                newChar = unichr(prechr[a]);
+                crypted.append(newChar);
+
+
+        crypted = str(crypted);
+        print crypted;
+
         return
 
 encrypt();
