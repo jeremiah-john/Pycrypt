0faeeab965bdfdd3d2f88646207ded318daff9bd
Jeremiah John <jeremiahjohn08706@gmail.com>
2017-03-01 13:02:01 -0500

    making progress on password

--- passwordTest.py
+++ passwordTest.py
@@ -1,5 +1,10 @@
 import cPickle as pickle
-
+setPass = raw_input("enter your desired password")
+checkPass = raw_input("re-enter password")
+while setPass != checkPass:
+    print "the password entries do not match!";
+    setPass = raw_input("enter your desired password");
+    checkPass = raw_input("re-enter password");
 key = {9, 6, 5, 0}
 
 pickle.dump(key, open( "key.p", "wb" ))
