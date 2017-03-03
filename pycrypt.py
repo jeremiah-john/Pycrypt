2f93e763346c6e67502a5ec36ee6a99badb949b7
Jeremiah John <jeremiahjohn08706@gmail.com>
2017-03-03 00:51:32 -0500

    by golly, it works!

--- pycrypt.py
+++ pycrypt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import os
 import base64
 import cPickle as pickle
 import random
@@ -49,14 +50,72 @@ def encrypt():
                 passwd = raw_input("Please set a password for your key file:");
                 passCheck = raw_input("enter password again:");
 
+
         passwd = passwd;
-        key = str(key);
         pickle.dump(key, open("key.p", "wb+"));
         encPass = base64.b64encode(passwd);
         passFile = open("passFile.txt", "wb+");
         passFile.write(encPass);
         newMsg.close();
         passFile.close();
-        return
+        return;
 
-encrypt();
+def decrypt():
+        message = open("out.txt", "rb");
+        human = open("message.txt", "wb+");
+        setPass = open("passFile.txt", "rb");
+        passStat = os.stat('passFile.txt');
+        passSize = passStat.st_size;
+        passLine = setPass.readline(passSize);
+        passWord = base64.b64decode(passLine);
+        
+        passCheck = raw_input("please enter your password to decrypt the file:");
+
+        while passCheck != passWord:
+                print ("Password Incorrect!");
+                passCheck = raw_input("please enter your password to decrypt the file");
+        key = pickle.load( open("key.p", "rb"));
+
+        messStat = os.stat('out.txt');
+        messSize = messStat.st_size;
+        fullMess = message.readline(messSize);
+        manipulateEnc = list(fullMess);
+
+        i = 0;
+        bigNums = [];
+        for letter in manipulateEnc:
+                noombar = ord(manipulateEnc[i]);
+                bigNums.append(noombar);
+                i = i + 1;
+
+        i = 0;
+        numsPreChr = [];
+        for data in bigNums:
+                together = bigNums[i] - key[i];
+                if together < 0:
+                        together = together + 225;
+                numsPreChr.append(together);
+                i = i + 1;
+        i = 0;
+        readable = [];
+        for num in numsPreChr:
+                origText = chr(numsPreChr[i]);
+                readable.append(origText);
+                i = i + 1;
+
+        unencrypted = ''.join(readable);
+        human.write(unencrypted);
+        message.close();
+        human.close();
+        setPass.close();
+        return;
+
+print ("Welcome to Pycrypt! The simple, light cipher!")
+
+usage = raw_input("Would you like to encrypt, or decrypt?")
+        
+if usage == "encrypt":
+        encrypt();
+elif usage == "decrypt":
+        decrypt();
+                                        
